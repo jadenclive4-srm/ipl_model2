f706be039b75dad9da225fcc0b7c9b3ba5ea874c
Clive Jaden <User@SRMLAP293.srmtchn.co.in>
2026-05-07 12:19:45 +0530

    pie bar fixes

--- backend/src/main/java/com/ipl/config/FrontendResourceConfig.java
+++ backend/src/main/java/com/ipl/config/FrontendResourceConfig.java
@@ -11,7 +11,7 @@ public class FrontendResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/static/");
+                .addResourceLocations("classpath:/static/");
         
         registry.addResourceHandler("/logos/**")
                 .addResourceLocations("classpath:/static/logos/");
